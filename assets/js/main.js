26af1567b11d1248baeb2ccef4f6ac1e90ef541c
Aaveg Gupta <aaveg.guptacs2018@indoreinstitute.com>
2021-09-27 18:38:41 +0530

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+setInterval(() => {
+  date = new Date();
+  hrTime = date.getHours();
+  minTime = date.getMinutes();
+  secTime = date.getSeconds();
+  day = date.getDay();
+  hrRotation = 30 * hrTime + minTime / 2;
+  minRotation = 6 * minTime;
+  secRotation = 6 * secTime;
+
+  hr.style.transform = `rotateZ(${hrRotation}deg)`;
+  min.style.transform = `rotateZ(${minRotation}deg)`;
+  sec.style.transform = `rotateZ(${secRotation}deg)`;
+
+  let week = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']
+
+  if(hrTime == 0){hrTime = 12}
+  if(hrTime < 10){hrTime = `0${hrTime}`}
+
+  if(minTime < 10){minTime = `0${minTime}`}
+
+  hrT.innerHTML = `${hrTime}`
+  hrTO.innerHTML = `${hrTime}:`
+  minT.innerHTML = `${minTime}`
+  dayT.innerHTML = `${week[day]}`
+  
+}, 1000);
